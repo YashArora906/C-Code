fd9f0075c797863bd1d8f7366c5f8553275dc60b
Yash Arora <85866499+YashArora906@users.noreply.github.com>
2023-12-10 01:24:07 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    if(n == 0){
+     return 1;
+    }
+
+    return (n * fact(n-1));
+    
+}
+
+int main(){
+
+    int num;
+
+    cout << "Enter a positive integer"<< endl;
+    cin>>num;
+
+    cout << "Factorial of " << num << " is " <<fact(num)<< endl;
+
+    return 0;
+
+}
