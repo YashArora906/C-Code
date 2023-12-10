4206e3e16327c14a189e0e60048c51bf160545d9
Yash Arora <85866499+YashArora906@users.noreply.github.com>
2023-12-11 01:26:57 +0530

    Update overloadingoperator.cpp

--- overloadingoperator.cpp
+++ overloadingoperator.cpp
@@ -1,45 +1,98 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+// class complex{
+//     float x;
+//     float y;
+//      public:
+//      complex(){}
+//      complex (float real, float imag){
+//         x = real;
+//         y = imag;
+//      }
+//      complex operator+(complex);
+//      void display(void);
+// };
+
+// complex complex :: operator+(complex c){
+//     complex temp;
+//     temp.x = x + c.x;
+//     temp.y = y + c.y;
+//     return(temp);
+// }
+// void complex :: display(void){
+//     cout << x << " + j " << y <<endl;
+// }
+
+// int main(){
+
+//     complex c1,c2,c3;
+//     c1 = complex(2.5, 3.5);
+//     c2  =complex(4.5, 6.7);
+//     c3 = c1+c2;
+
+//     cout << "C1 = ";
+//     c1.display();
+
+//     cout << "C2 = ";
+//     c2.display();
+
+//     cout << "C3 = ";
+//     c3.display();
+
+//     return 0;
+
+// }
+
+#include <iostream>
+
 using namespace std;
 
-class complex{
-    float x;
-    float y;
-     public:
-     complex(){}
-     complex (float real, float imag){
-        x = real;
-        y = imag;
-     }
-     complex operator+(complex);
-     void display(void);
+class Time
+{
+    int hours;
+    int minutes;
+
+public:
+    Time operator+(const Time &t) const
+    {
+        Time result;
+        result.minutes = minutes + t.minutes;
+        result.hours = hours + t.hours + result.minutes / 60;
+        result.minutes %= 60;
+        return result;
+    }
+
+    void gettime(int h, int m)
+    {
+        hours = h;
+        minutes = m;
+    }
+
+    void puttime() const
+    {
+        cout << hours << " hours and ";
+        cout << minutes << " minutes" << "\n";
+    }
 };
 
-complex complex :: operator+(complex c){
-    complex temp;
-    temp.x = x + c.x;
-    temp.y = y + c.y;
-    return(temp);
-}
-void complex :: display(void){
-    cout << x << " + j " << y <<endl;
-}
+int main()
+{
+    Time T1, T2, T3;
 
-int main(){
+    T1.gettime(2, 45);
+    T2.gettime(3, 30);
 
-    complex c1,c2,c3;
-    c1 = complex(2.5, 3.5);
-    c2  =complex(4.5, 6.7);
-    c3 = c1+c2;
+    T3 = T1 + T2;
 
-    cout << "C1 = ";
-    c1.display();
+    cout << "T1 = ";
+    T1.puttime();
 
-    cout << "C2 = ";
-    c2.display();
+    cout << "T2 = ";
+    T2.puttime();
 
-    cout << "C3 = ";
-    c3.display();
+    cout << "T3 = ";
+    T3.puttime();
 
     return 0;
-
 }
